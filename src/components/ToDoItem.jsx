f01c1582c9a9bb1e6aae3025d6cbb3e2039fb5a7
disensee <disensee27@gmail.com>
2021-04-18 16:29:47 -0500

    Crossing out todolist item when clicked on

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ToDoItem(props){
-    return <li>{props.item}</li>;
+    const [isComplete, setCompleted] = useState(false);
+    
+    function toggleComplete(){
+        setCompleted(prevValue=>!prevValue);
+    }
+
+    return (
+    <div>
+        <li onClick={toggleComplete} style={{textDecoration : isComplete ? "line-through" : ""}}>{props.item}</li>
+    </div>
+    );
 }
 
 export default ToDoItem;
